7d1bcd9839ab61de76512bbe0bf4d0fdf0efeea7
isuke <isuke770@gmail.com>
2019-01-12 21:22:06 +0900

    feat: add version option

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+const test = require('ava')
+const execSync = require('child_process').execSync
+const path = require('path')
+
+test('--version', (t) => {
+  const output = execSync(`${path.join(__dirname, '../lib/index.js')} --version`)
+    .toString()
+    .trim()
+
+  t.is(output, '0.0.1')
+})
